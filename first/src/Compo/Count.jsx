ba920cb0968a6b8d12f94565c8fa9e4cb027aa0b
Amruth96 <amruth.k@innovatorstech.com>
2024-12-05 16:29:57 +0530

    Added Reset button

--- first/src/Compo/Count.jsx
+++ first/src/Compo/Count.jsx
@@ -9,11 +9,15 @@ function Count(){
    const handlecounter=()=>{
     setCount(count-1)
    }
+   const handleClear=()=>{
+    setCount(0) 
+   }
     return(
         <div>
         <p>{count}</p>
        <button onClick={handlecount}>Increment</button>
        <button onClick={handlecounter}>decrement</button>
+       <button onClick={handleClear}>Reset</button>
         </div>
     )
 }
